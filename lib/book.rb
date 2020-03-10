6a4933489a85e5fde7039b233bf42a8274566866
Christine Bumpous <bumpouce@gmail.com>
2020-03-10 23:33:38 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,10 @@
 class Book
   
-  def initialize (title, author, page_count)
+  def initialize (title, author, page_count, genre)
     @title = title
+    @author = author
+    @page_count = page_count
+    @genre = genre
   end
 
   def title
